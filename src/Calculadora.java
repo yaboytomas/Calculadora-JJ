9223ecc95fec84464a3f6eeec79a3b68f3603e92
yaboytomas <tomasszabo94@gmail.com>
2024-09-19 16:15:19 -0300

    2nd commit

---
+++ src/Calculadora.java
@@ -0,0 +1,34 @@
+
+public class Calculadora {
+
+    //Atributos
+
+    //Metodos
+    public int sumarInt(int a, int b) {
+        return a + b;
+    }
+
+    public String sumarString(String a, String b) {
+        return a + b;
+    }
+
+    public double sumarDouble(double a, double b) {
+        return a + b;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,51 @@
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
 
+        //Instancias
+        Calculadora calculadora = new Calculadora();
 
 
+        System.out.println("");
+        System.out.println("--------------------");
+        System.out.println("     Calculadora    ");
+        System.out.println("--------------------");
+
+        //Sumar dos numeros enteros
+        System.out.println("");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese el primer numero entero: ");
+        int a = scanner.nextInt();
+        System.out.println("Ingrese el segundo numero entero: ");
+        int b = scanner.nextInt();
+        System.out.println("La suma de los numeros enteros es: " + calculadora.sumarInt(a, b));
+        System.out.println("--------------------");
+
+        //Sumar dos numeros decimales
+        System.out.println("");
+        Scanner scanner2 = new Scanner(System.in);
+        System.out.println("Ingrese el primer numero decimal: ");
+        double c = scanner2.nextDouble();
+        System.out.println("Ingrese el segundo numero decimal: ");
+        double d = scanner2.nextDouble();
+        System.out.println("La suma de los numeros decimales es: " + calculadora.sumarDouble(c, d));
+        System.out.println("--------------------");
+
+        //Sumar dos cadenas de texto
+        System.out.println("");
+        Scanner scanner3 = new Scanner(System.in);
+        System.out.println("Ingrese la primera cadena de texto: ");
+        String e = scanner3.nextLine();
+        System.out.println("Ingrese la segunda cadena de texto: ");
+        String f = scanner3.nextLine();
+        System.out.println("La suma de las cadenas de texto es: " + calculadora.sumarString(e, f));
+        System.out.println("--------------------");
+
+        System.out.println(" Adios ! ");
+        System.out.println("--------------------");
 
 
 
